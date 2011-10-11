d3fbd8bbbe1d069d3a64d0a3e8ffdc6e12515dfc
zinkem <zinkem@gmail.com>
2011-10-11 15:44:52 -0700

    Added rest of project files

--- NodeChat.js
+++ NodeChat.js
@@ -1,7 +1,39 @@
 http = require('http');
+url = require('url');
+path = require('path');
+fs = require('fs');
+io = require('socket.io');
 
-s = http.createServer(function(req, res){
+webServ = http.createServer(function(req, res){
+	var uri = url.parse(req.url).pathname;
+	var abspath = path.join(process.cwd(), uri);
+
+	if(req.url == '/'){
+	    abspath += 'index.html';
+	}
+
+	path.exists(abspath, function(exists){
+		if(!exists){
+		    res.writeHead(404, {"Content-Type":"text/html"});
+		    res.write('<html><body>404</body></html>');
+		    res.end('');
+		    return;
+		}
+
+		fs.readFile(abspath, "binary", function(err, file){
+			res.writeHead(200, {"Content-Type":"text/html"});
+			res.write(file, "binary");
+			res.end('');
+		    });
+	    });
 	
     });
 
-s.listen(8000);
+webServ.listen(8000);
+
+
+var socket = io.listen(webServ);
+
+socket.sockets.on('connection', function(client){
+	console.log("connection works!");
+    });
---
+++ chatclient.js
@@ -0,0 +1,4 @@
+function init(){
+    document.body.innerHTML = "Good jorb!";
+    var socket = new io.connect('http://localhost');
+}
